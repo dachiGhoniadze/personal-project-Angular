67eac3d8bfcbba1659a9ea660a1d48961681c7bc
dachigh <d_ghoniadze@cu.edu.ge>
2023-02-11 16:42:42 +0400

    created routing for all components

--- cv-app/src/app/app-routing.module.ts
+++ cv-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PrivateInfoComponent } from './private-info/private-info.component';
+import {ExpInfoComponent} from './exp-info/exp-info.component'
+import { EduInfoComponent } from './edu-info/edu-info.component';
+import { FinalComponent } from './final/final.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'private-info', component: PrivateInfoComponent },
+  { path: 'exp-info', component: ExpInfoComponent },
+  { path: 'edu-info', component: EduInfoComponent },
+  { path: 'final', component: FinalComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
